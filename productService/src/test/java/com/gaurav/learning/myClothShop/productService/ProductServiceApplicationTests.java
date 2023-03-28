48137cd3a5bc6c103763603dc1fc5d77e189a551
Gaurav SUMAN <gaurav.suman@amadeus.com>
2023-03-28 16:30:10 +0530

    Initial commit for product service

---
+++ productService/src/test/java/com/gaurav/learning/myClothShop/productService/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaurav.learning.myClothShop.productService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
